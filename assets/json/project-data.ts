4d068e407098dec2e84774a2bbbe09ed5b856041
AugusCav <57458405+AugusCav@users.noreply.github.com>
2024-09-10 17:29:10 -0300

    Update site

---
+++ assets/json/project-data.ts
@@ -0,0 +1,57 @@
+export const PROJECT_DATA = [
+  {
+    id: 1,
+    titulo: 'ChessTourney - Gestor de torneos de ajedrez',
+    stack: ['SQL Server', 'Angular', 'NET Core', 'Bootstrap', 'Stockfish'],
+    descripcion:
+      'Para el proyecto final de mi carrera —la Tecnicatura en Programación de la Universidad Tecnológica Nacional de Córdoba— creé una web app para la gestión de torneos de ajedrez de un modo sencillo y amigable. La aplicación consistía en usuarios de tipo "organizador" y usuarios de tipo "participante". El rol de organizador permitía crear torneos, agregando los detalles necesarios para que los usuarios que quisieran competir fueran capaces de encontrar fácilmente el torneo según las características que prefirieran. La aplicación ofrecía además la capacidad de analizar partidas jugadas en los torneos a partir de un motor de ajedrez.',
+    imagen: '../../../assets/img/chesstourney.png',
+    alt: 'App de analisis de partida de ajedrez',
+    demo: '',
+    codigo: '',
+  },
+  {
+    id: 2,
+    titulo: 'Blackjack',
+    stack: ['Angular', 'Bootstrap'],
+    descripcion:
+      'Un simple juego de Blackjack creado como webapp para jugar contra la PC.',
+    imagen: '../../../assets/img/blackjack.jpeg',
+    alt: 'Imagen de partida de Blackjack',
+    demo: 'https://auguscav.github.io/black-jack/',
+    codigo: '',
+  },
+  {
+    id: 3,
+    titulo: 'Generador de ideas de coding',
+    stack: ['Angular', 'Tailwind'],
+    descripcion:
+      'Este es un generador de ideas de programación basado en la restricción de "tipos" y "temas". El generador simplemente elige aleatoriamente un tipo, que refiere al tipo de proyecto, y un tema, que refiere a una temática general a la que orientar el proyecto. La idea es que se utilice para obtener inspiración para la creación de proyectos, como una forma de brainstorming.',
+    imagen: '../../../assets/img/generador.jpeg',
+    alt: 'Imagen de generador de ideas de coding',
+    demo: 'https://auguscav.github.io/generador-ideas-code/',
+    codigo: '',
+  },
+  {
+    id: 4,
+    titulo: 'Gestor de compra/ventas',
+    stack: ['Java', 'Java Swing', 'SQL Server', 'FlatLaF GUI'],
+    descripcion:
+      'Proyecto para una materia de la universidad, donde trabajamos junto con algunos compañeros creando una aplicación de escritorio. Es un simple gestor de compra de insumos y venta de helados para una heladería ficticia.',
+    imagen: '../../../assets/img/heladeria.png',
+    alt: 'Imagen de gestor de compra/ventas',
+    demo: '',
+    codigo: '',
+  },
+  {
+    id: 5,
+    titulo: 'Calculadora',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    descripcion:
+      'Mini proyecto de calculadora con operaciones matemáticas básicas.',
+    imagen: '../../../assets/img/calculadora.png',
+    alt: 'Imagen de calculadora web',
+    demo: 'https://auguscav.github.io/calculadora/',
+    codigo: '',
+  },
+];
---
+++ assets/json/timeline-data.ts
@@ -0,0 +1,49 @@
+export const TIMELINE_DATA = {
+  estudios: [
+    {
+      id: 'bachiller',
+      titulo: 'Bachiller en Economía y Administración',
+      localidad:
+        'Escuela Superior el Nacional - La Carlota, Córdoba, Argentina',
+      fecha: '2012 - 2017',
+      descripcion:
+        'Mi educación secundaria se enfocó en las áreas de economía, contabilidad y administración de empresas.',
+    },
+    {
+      id: 'ingenieria',
+      titulo: 'Ingeniería en Sistemas de Información',
+      localidad:
+        'Universidad Tecnológica Nacional - Córdoba, Córdoba, Argentina',
+      fecha: '2018 ~',
+      descripcion:
+        'Si bien no finalicé la carrera de ingeniero en Sistemas de Información, aprendí muchas cosas útiles para mi trabajo como programador, entre las que se destacan conocimientos en computación, algoritmos y conocimientos en Python.',
+    },
+    {
+      id: 'tecnicatura',
+      titulo: 'Tecnicatura en Programación',
+      localidad:
+        'Universidad Tecnológica Nacional - Córdoba, Córdoba, Argentina',
+      fecha: '2021-2023',
+      descripcion:
+        'La tecnicatura en Programación me proporcionó los conocimientos necesarios para el desarrollo de aplicaciones fullstack. Durante la carrera, aprendí a desarrollar sitios web utilizando HTML, CSS, y JavaScript, así como NodeJS, jQuery básico y Angular avanzado. También, adquirí experiencia programando REST APIs en el backend y aplicaciones nativas de Windows con C# y .NET, utilizando Winforms. Asimismo, me familiaricé con patrones de diseño y la creación y administración de bases de datos relacionales con SQL Server, PostgreSQL y MySQL, así como bases de datos no relacionales con MongoDB. Además, aprendí programación en JAVA utilizando Swing y Spring Boot para el backend. La carrera también me permitió familiarizarme con metodologías ágiles como Scrum y con el QA testing.',
+    },
+  ],
+  trabajos: [
+    {
+      id: 'pasantias-molinos',
+      titulo: 'Pasantías',
+      localidad: 'Molinos Marimbos - La Carlota, Córdoba, Argentina',
+      fecha: '2017',
+      descripcion:
+        'Tareas administrativas relacionadas a la contabilidad; organización de archivos, búsqueda de discrepancias en libros contables.',
+    },
+    {
+      id: 'ayudante-electricista',
+      titulo: 'Ayudante de electricista',
+      localidad: 'La Carlota, Córdoba, Argentina',
+      fecha: '2019-2020',
+      descripcion:
+        'Cableado, armado de toma corrientes, instalación de luminarias.',
+    },
+  ],
+};
